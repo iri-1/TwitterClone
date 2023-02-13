b5564f0cf110d0731e3eb6353fe7b447ed21b458
iri-1 <66905641+iri-1@users.noreply.github.com>
2023-02-14 00:29:56 +0900

    ユーザー画面

--- Views/sign-up.php
+++ Views/sign-up.php
@@ -21,7 +21,7 @@
             <input type="text" class="form-control" name="nickname" placeholder="ニックネーム" maxlength="50" required autofocus>
             <input type="text" class="form-control" name="name" placeholder="ユーザー名, 例:Techis221" maxlength="50" required>
             <input type="email" class="form-control" name="email" placeholder="メールアドレス" maxlength="254" required>
-            <input type="password" class="form-control" name="password" placeholder="パスワード" maxlength="50" required>
+            <input type="password" class="form-control" name="password" placeholder="パスワード" maxlength="128" required>
             <button class="w-100 btn btn-lg" type="submit">登録する</button>
             <p class="mt-3 mb-2"><a href="sign-in.php">ログインする</a></p>
             <p class="mt-2 mt-3 text-muted">&copy; 2023</p>
